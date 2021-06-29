9d6ae0e6090ae84c92e18bbc1d14fbb26501a721
Otávio de Paula <otaviopaulaferreira@gmail.com>
2021-06-29 15:16:50 -0300

    Update Autoload

--- site/autoload.php
+++ site/autoload.php
@@ -1,19 +1,6 @@
 <?php
     spl_autoload_register(function($nome_arquivo)
     {
-        // if(file_exists('Controllers/'.$nome_arquivo.'.php'))
-        // {
-        //     require 'Controllers/'.$nome_arquivo.'.php';
-        // }
-        // elseif(file_exists('Core/'.$nome_arquivo.'.php'))
-        // {
-        //     require 'Core/'.$nome_arquivo.'.php';
-        // }
-        // elseif(file_exists('Models/'.$nome_arquivo.'.php'))
-        // {
-        //     require 'Models/'.$nome_arquivo.'.php';
-        // }
-
         $diretorios = ['Models', 'Core', 'Controllers', 'Views'];
         foreach ($diretorios as $diretorio)
         {
@@ -29,8 +16,7 @@
             else
             {
                 $arquivo = (__DIR__.DIRECTORY_SEPARATOR.$diretorio.DIRECTORY_SEPARATOR.$nome_arquivo.'.php');
-            
-            
+              
                 if(file_exists($arquivo))
                 {
                     require $arquivo;
